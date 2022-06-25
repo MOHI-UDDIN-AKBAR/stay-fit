510effb4d3cccf6a98e1bb1a593c8e97f2a14f15
MOHI-UDDIN-AKBAR <mohiuddinakbar397720@gmail.com>
2022-06-26 00:28:26 +0200

    added & modified

--- src/components/AllCategory/AllCategory.jsx
+++ src/components/AllCategory/AllCategory.jsx
@@ -2,19 +2,42 @@ import React, { useContext } from "react";
 import "./AllCategory.css";
 import avatarMan from "../../assets/avatarMan.svg";
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+import ForNow from "./ForNow";
+
 const LeftArrow = () => {
   const { scrollPrev } = useContext(VisibilityContext);
-  return <div onClick={() => scrollPrev()}>previous</div>;
+
+  return (
+    <div onClick={() => scrollPrev()} className="right-arrow">
+      <img src={avatarMan} alt="right-arrow" style={{ width: "40px" }} />
+    </div>
+  );
 };
+
 const RightArrow = () => {
   const { scrollNext } = useContext(VisibilityContext);
-  return <div onClick={() => scrollNext()}>previous</div>;
+
+  return (
+    <div
+      onClick={() => scrollNext()}
+      className="left-arrow"
+      style={{ width: "40px" }}
+    >
+      <img src={avatarMan} alt="right-arrow" style={{ width: "40px" }} />
+    </div>
+  );
 };
+
 const AllCategory = ({ bodyParts }) => {
   return (
     <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-      {bodyParts?.map((item) => (
-        <div className="Category" key={item.id}>
+      {bodyParts.map((item, id) => (
+        <div
+          key={item.id || item}
+          itemId={item.id || item}
+          title={item.id || item}
+          className="Category"
+        >
           <div className="image">
             <img src={avatarMan} alt={item} />
           </div>
